883471f8f102e6c948313f0a1417b569c6d03773
Miro Dojkic <mdojkic@gmail.com>
2020-09-13 22:49:13 +0200

    Add ‘form field should invoke setValue’ ✅

--- tests/unit/FormField.spec.js
+++ tests/unit/FormField.spec.js
@@ -1,3 +1,5 @@
+import { fireEvent, render } from '@testing-library/vue';
+import { flushPromises } from '../utils';
 import FormField from '@/FormField.vue';
 import { mount } from '@vue/test-utils';
 
@@ -32,5 +34,19 @@ describe('Display props', () => {
     });
     const input = wrapper.find('[data-testid="default-input"]');
     expect(input.element.placeholder).toBe('Test placeholder');
+  });
+
+  it('should invoke injected setValue', async () => {
+    const values = { test: '' };
+    const setValue = jest.fn();
+    const provide = { values, setValue };
+    const wrapper = render(FormField, {
+      provide,
+      propsData: { name: 'test' }
+    });
+    const input = wrapper.getByTestId('default-input');
+    await fireEvent.update(input, 'new value');
+    await flushPromises();
+    expect(setValue).toBeCalledTimes(1);
   });
 });
