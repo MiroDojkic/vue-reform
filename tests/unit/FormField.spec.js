30e05ac08449b2e11d5a5ea8b0f9a4fc62cc3cf9
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-09-13 11:14:59 +0200

    Add first test cases

--- tests/unit/FormField.spec.js
+++ tests/unit/FormField.spec.js
@@ -1,7 +1,50 @@
-// import { shallowMount } from '@vue/test-utils';
+import FormField from '@/FormField.vue';
+import { mount } from '@vue/test-utils';
 
-describe('test example block', () => {
-  test('first test example', () => {
-    expect(true).toBe(true);
+describe('Receive props', () => {
+  const provide = {
+    values() {
+      return {};
+    },
+    setValue() {
+      return () => {};
+    }
+  };
+  it('should receive name prop', () => {
+    const wrapper = mount(FormField, {
+      provide,
+      propsData: {
+        name: 'testname'
+      }
+    });
+    expect(wrapper.props('name')).toBe('testname');
+  });
+  it('default label prop value should be null', () => {
+    const wrapper = mount(FormField, {
+      provide,
+      propsData: {
+        name: 'test'
+      }
+    });
+    expect(wrapper.props('label')).toBe(null);
+  });
+  it('default placeholder prop value should be null', () => {
+    const wrapper = mount(FormField, {
+      provide,
+      propsData: {
+        name: 'test'
+      }
+    });
+    expect(wrapper.props('placeholder')).toBe(null);
+  });
+  it('should display label prop text', () => {
+    const wrapper = mount(FormField, {
+      provide,
+      propsData: {
+        name: 'test',
+        label: 'Test label'
+      }
+    });
+    expect(wrapper.text()).toContain('Test label');
   });
 });
