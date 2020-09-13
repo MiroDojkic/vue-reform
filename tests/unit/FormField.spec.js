928eff3d0bcf4eeb56ca27161d56b5956c0fad3e
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-09-13 11:18:16 +0200

    Add newline before each test case

--- tests/unit/FormField.spec.js
+++ tests/unit/FormField.spec.js
@@ -10,6 +10,7 @@ describe('Receive props', () => {
       return () => {};
     }
   };
+
   it('should receive name prop', () => {
     const wrapper = mount(FormField, {
       provide,
@@ -19,6 +20,7 @@ describe('Receive props', () => {
     });
     expect(wrapper.props('name')).toBe('testname');
   });
+
   it('default label prop value should be null', () => {
     const wrapper = mount(FormField, {
       provide,
@@ -28,6 +30,7 @@ describe('Receive props', () => {
     });
     expect(wrapper.props('label')).toBe(null);
   });
+
   it('default placeholder prop value should be null', () => {
     const wrapper = mount(FormField, {
       provide,
@@ -37,6 +40,7 @@ describe('Receive props', () => {
     });
     expect(wrapper.props('placeholder')).toBe(null);
   });
+
   it('should display label prop text', () => {
     const wrapper = mount(FormField, {
       provide,
